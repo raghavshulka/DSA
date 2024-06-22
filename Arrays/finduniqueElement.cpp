e3902e480415b774f1a38e300efaaedfcb102c6c
HIMANSHU SHUKLA <120308745+raghavshulka@users.noreply.github.com>
2024-06-23 00:49:51 +0530

    .

---
+++ Arrays/finduniqueElement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int findUniqueElement(int arr[], int n) {
+    int element = 0;  // Initialize element to 0
+    for (int i = 0; i < n; i++) {
+        element = element ^ arr[i];
+    }
+    return element;  // Return the result
+}
+
+int main() {
+    int n = 7;  
+    int arr[n] = {1, 1, 5, 5, 3, 3, 7};  
+    int result = findUniqueElement(arr, n);
+    cout << "The unique element is: " << result << endl;
+    return 0;
+}
